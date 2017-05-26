acc706b2345bb7f1b1bb9b1168ba27832fd7f557
HustLion <hustlionm@qq.com>
2017-05-26 21:18:52 +0800

    add project

---
+++ first_java_mvn_project/src/main/java/com/hustlion/first_java_mvn_project/App.java
@@ -0,0 +1,13 @@
+package com.hustlion.first_java_mvn_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
